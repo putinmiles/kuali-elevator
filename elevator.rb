0b94543ce0e46893c77e129c842ea4985bef09a5
JD Skinner <yu.kagi@gmail.com>
2017-05-03 20:19:47 -0600

    Rethinking how the elevator should function
    If someone were to be traveling from floor 1 to 20, and someone from
    floor 15 needs to go to floor 19 (or 20-25 assuming a max of 25), and
    said elevator is the closest to floor 15 at the time of the request,
    it should be the assigned elevator. However, a "target_floor" then
    becomes "target_floors".
    
    Currently working on how to handle the elevator traversal of floors.

--- elevator.rb
+++ elevator.rb
@@ -4,22 +4,29 @@ class Elevator
   def initialize(number_of_floors, id)
     @floors = number_of_floors
     @current_floor = 1
-    @target_floor = 1
+    @target_floors = []
     @id = id
     @in_transit = false
   end
 
   def proceed_to_target_floor
     distance_to_target_floor.times do |floor|
+      if @target_floor < @current_floor
+        move_up
+      else
+        move_down
+      end
     end
   end
 
   def move_up
     puts "Elevator #{id} Going up!"
+    @current_floor += 1
   end
 
   def move_down
     puts "Elevator #{id} Going down!"
+    @current_floor -= 1
   end
 
   def open_doors
