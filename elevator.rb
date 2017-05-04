c1d37cc059c74182e71e55902d1a7aab14fd85c0
JD Skinner <yu.kagi@gmail.com>
2017-05-03 21:03:50 -0600

    Working out elevator behavior
    Currently working on how an elevator should move up/down, how to
    determine whether an elevator is in transit or idle, and the
    interactions between the ElevatorController and Elevator.

--- elevator.rb
+++ elevator.rb
@@ -6,35 +6,41 @@ class Elevator
     @current_floor = 1
     @target_floors = []
     @id = id
-    @in_transit = false
+    @doors_open = false
+    # NOTE: What should constitute a trip?
+    @trips = 0
   end
 
-  def proceed_to_target_floor
-    distance_to_target_floor.times do |floor|
-      if @target_floor < @current_floor
-        move_up
-      else
-        move_down
-      end
-    end
+  def add_floor(floor)
+    @target_floors << floor
+  end
+
+  def display_current_floor
+    "Elevator #{@id} at #{current_floor}"
   end
 
   def move_up
-    puts "Elevator #{id} Going up!"
     @current_floor += 1
+    display_current_floor
   end
 
   def move_down
-    puts "Elevator #{id} Going down!"
     @current_floor -= 1
+    display_current_floor
   end
 
   def open_doors
     puts "Elevator #{id} Opening Doors"
+    @doors_open = true
   end
 
   def close_doors
     puts "Elevator #{id} Closing Doors"
+    @doors_open = false
+  end
+
+  def in_transit?
+    !@doors_open && !@target_floors.empty? ? true : false
   end
 
   def distance_to_target_floor(target)
