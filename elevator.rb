2c443789d4f7b496ed16e24db0511274729037c4
JD Skinner <yu.kagi@gmail.com>
2017-05-03 20:02:35 -0600

    Further development of the Elevator and ElevatorControllers
    Currently thinking that the ElevatorController will process incoming
    floor requests, then determine which elevator to delegate to. An empty
    elevator will then travel to the desired floor, reporting its progress
    along the way.

--- elevator.rb
+++ elevator.rb
@@ -4,26 +4,33 @@ class Elevator
   def initialize(number_of_floors, id)
     @floors = number_of_floors
     @current_floor = 1
+    @target_floor = 1
     @id = id
+    @in_transit = false
+  end
+
+  def proceed_to_target_floor
+    distance_to_target_floor.times do |floor|
+    end
   end
 
   def move_up
-    puts "Going up!"
+    puts "Elevator #{id} Going up!"
   end
 
   def move_down
-    puts "Going down!"
+    puts "Elevator #{id} Going down!"
   end
 
   def open_doors
-    puts "Opening Doors"
+    puts "Elevator #{id} Opening Doors"
   end
 
   def close_doors
-    puts "Closing Doors"
+    puts "Elevator #{id} Closing Doors"
   end
 
   def distance_to_target_floor(target)
-    
+    (target - @current_floor).abs
   end
 end
--- elevator_controller.rb
+++ elevator_controller.rb
@@ -6,4 +6,9 @@ class ElevatorController
       Elevator.new(@number_of_floors, id)
     end
   end
+
+  def process_incoming_request(floor)
+    # determine which elevator is closest
+    # send elevator to target floor
+  end
 end
